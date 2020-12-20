6874aa64483b5a6a78b4ad56140e59d0e5a0f6e8
momchilasenov <momchil276@gmail.com>
2020-12-20 18:36:14 +0200

    First commit

---
+++ 01.BasicCSharpWebCrawler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace BasicCSharpWebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string laptopbg = "";
+            using (StreamReader reader =
+                new StreamReader("C:\\Users\\Momchil Asenov\\source\\repos\\BasicCSharpWebCrawler\\BasicCSharpWebCrawler\\pageSource.txt"))
+            {
+                laptopbg = reader.ReadToEnd();
+            }
+
+            laptopbg = Regex.Replace(laptopbg, @"\n", "");
+
+            Regex regex = new Regex(@"<h2>(?<model>.*?)<\/h2>.*?<li>(?<cpu>.*?)<\/li>.*?<span class='price'>(?<price>.*?)<");
+
+            MatchCollection matches = regex.Matches(laptopbg);
+            int counter = 0;
+
+            foreach (Match match in matches)
+            {
+                string model = match.Groups["model"].Value;
+                string cpu = match.Groups["cpu"].Value;
+                int price = int.Parse(match.Groups["price"].Value);
+                counter++;
+
+                if (match.Groups["model"].Value.Contains('>') || match.Groups["cpu"].Value.Contains('>'))
+                {
+                    counter--;
+                }
+                else
+                {
+                    Console.WriteLine($"Laptop #{counter} {model}");
+                    Console.WriteLine($"CPU: {cpu}");
+                    Console.WriteLine($"Price: {price}");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
